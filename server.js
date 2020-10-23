41ef5d34b7fcccfad55b9cf3ff93ffcc4382ce2c
Michael <Michael@Michaels-MacBook-Pro.local>
2020-10-23 15:13:19 +0100

    chore: Minor update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'app/public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
